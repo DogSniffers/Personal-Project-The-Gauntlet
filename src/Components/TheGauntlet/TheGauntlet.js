46074c9a2737b75ad338fe363fbb2247fc690741
DogSniffers <DogSniffers@users.noreply.github.com>
2020-03-04 11:28:56 -0700

    Random works

--- src/Components/TheGauntlet/TheGauntlet.js
+++ src/Components/TheGauntlet/TheGauntlet.js
@@ -58,16 +58,76 @@ class TheGauntlet extends React.Component{
     }
     componentDidMount(){
         axios.get('/api/classes').then(res => {
-            this.setState({classList:res.data})}
+            this.setState({classList:res.data})
+            let classListLength = this.state.classList.length
+            let class1 = Math.floor(Math.random()*classListLength)
+            let class2 = Math.floor(Math.random()*classListLength)
+            let class3 = Math.floor(Math.random()*classListLength)
+            if(class1 === class2){
+                class2 += 1}
+            if(class1 === class3){
+                class3 += 1}
+            if(class2 === class3){
+                class3 +=1}
+            
+            console.log(class1, class2, class3)
+            
+            }
         )
     }
 
     
 
 
-    handleClassClick = () => {
-        this.setState({classClick:true})
+    selectedClass1 = () => {
+        this.setState({
+            classClick:true,
+            selectedClassName: this.state.class1Name,
+            selectedClassDesc: this.state.class1Desc,
+            selectedClassattack1Name: this.state.class1attack1Name,
+            selectedClassattack1Damage: this.state.class1attackDamage,
+            selectedClassattack1Type: this.state.class1attack1Type,
+            selectedClassattack1Cost: this.state.class1attack1Cost,
+            selectedClassattack2Name: this.state.class1attack2Name,
+            selectedClassattack2Damage: this.state.class1attack2Damage,
+            selectedClassattack2Type: this.state.class1attack2Type,
+            selectedClassattack2Cost: this.state.class1attack2Cost,
+        })
     }
+
+    selectedClass2 = () => {
+        this.setState({
+            classClick:true,
+            selectedClassName: this.state.class2Name,
+            selectedClassDesc: this.state.class2Desc,
+            selectedClassattack1Name: this.state.class2attack1Name,
+            selectedClassattack1Damage: this.state.class2attackDamage,
+            selectedClassattack1Type: this.state.class2attack1Type,
+            selectedClassattack1Cost: this.state.class2attack1Cost,
+            selectedClassattack2Name: this.state.class2attack2Name,
+            selectedClassattack2Damage: this.state.class2attack2Damage,
+            selectedClassattack2Type: this.state.class2attack2Type,
+            selectedClassattack2Cost: this.state.class2attack2Cost,
+        })
+    }
+
+    selectedClass3 = () => {
+        this.setState({
+            classClick:true,
+            selectedClassName: this.state.class3Name,
+            selectedClassDesc: this.state.class3Desc,
+            selectedClassattack1Name: this.state.class3attack1Name,
+            selectedClassattack1Damage: this.state.class3attackDamage,
+            selectedClassattack1Type: this.state.class3attack1Type,
+            selectedClassattack1Cost: this.state.class3attack1Cost,
+            selectedClassattack2Name: this.state.class3attack2Name,
+            selectedClassattack2Damage: this.state.class3attack2Damage,
+            selectedClassattack2Type: this.state.class3attack2Type,
+            selectedClassattack2Cost: this.state.class3attack2Cost,
+        })
+    }
+
+    
 
     render(){
         console.log(this.state.classList)
@@ -77,9 +137,9 @@ class TheGauntlet extends React.Component{
                     
                 <div>
                 <>
-            <div onClick={this.handleClassClick}>Class 1</div>
-            <div onClick={this.handleClassClick}>Class 2</div>
-            <div onClick={this.handleClassClick}>Class 3</div>
+            <div onClick={this.selectedClass1}>Class 1</div>
+            <div onClick={this.selectedClass2}>Class 2</div>
+            <div onClick={this.selectedClass3}>Class 3</div>
             {this.state.classClick === false ? (
                 null
             ):(
@@ -100,6 +160,11 @@ class TheGauntlet extends React.Component{
 
                 </div>
                 )}
+                <div>Player Div
+                    <p>Class Name</p>
+                    <p>Class Health</p>
+                    <p></p>
+                </div>
                 <div>Attacks Div</div>
                 <div>Monster Div</div>
             </div>
