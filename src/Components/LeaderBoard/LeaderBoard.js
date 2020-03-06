ff82515bd5f4f5dee47d730a2892a08a7f858b58
DogSniffers <DogSniffers@users.noreply.github.com>
2020-03-06 13:48:21 -0700

    Leardbord displays runs

--- src/Components/LeaderBoard/LeaderBoard.js
+++ src/Components/LeaderBoard/LeaderBoard.js
@@ -7,7 +7,7 @@ class LeaderBoard extends React.Component{
     constructor(){
         super()
         this.state = {
-            leaderboard:[],
+            leaderboard:[{username:'Test',score:100,floors:20,death:'Test death text'},{username:'Test 2',score:200,floors:40,death:'Test death text'}],
 
 
         }
@@ -21,6 +21,16 @@ class LeaderBoard extends React.Component{
         return(
             <div>
                 <h1>LeaderBoard</h1>
+                {this.state.leaderboard.map(score => {
+                    return(
+                    <div>
+                        <p>{score.username}</p>
+                        <p>{score.points}</p>
+                        <p>{score.floors}</p>
+                        <p>{score.death}</p>
+                        </div>
+                    )
+                })}
                 <button onClick={() => this.props.history.push('/dashboard')}>Start Run</button>
             </div>
         )
