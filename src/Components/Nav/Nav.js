2e75a11eb29a54630e5e55f05d70f04d6960654a
DogSniffers <DogSniffers@users.noreply.github.com>
2020-03-15 20:31:17 -0600

    Styled

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Axios from 'axios'
 import {withRouter} from 'react-router-dom'
+import {connect} from 'react-redux'
+import './Nav.css'
 
 class Nav extends React.Component{
 
@@ -14,11 +16,18 @@ class Nav extends React.Component{
     render(){
         return(
             <div>
-            <h1 className='pageTitle'>The Gauntlet</h1>
-            <button onClick={this.logout}>Logout</button>
+            <h1 className={`${this.props.color}title`}>The Gauntlet</h1>
+            <button onClick={this.logout} className={`${this.props.color}button`}>Logout</button>
             </div>
         )
     }
 }
+const mapStateToProps = reduxState =>{
+    return{
+        reduxState,
+        color: reduxState.colorReducer.color
 
-export default withRouter(Nav)
+    }
+}
+
+export default connect(mapStateToProps)(withRouter(Nav))
